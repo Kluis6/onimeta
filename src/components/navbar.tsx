eb3ebc26100abf19ca2e9d97fcd346fa7f097d37
kluis6 <luislmorningstar@gmail.com.br>
2024-08-23 06:57:44 -0300

    2

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,100 @@
+import Link from "next/link";
 import { BsBarChartLine } from "react-icons/bs";
-
 
 export default function Navbar() {
   return (
-    <div className="bg-gradient-to-r from-blue-500 to-cyan-500 w-screen">
-      <div className=" px-4 py-2">
+    <div className="bg-gradient-to-r from-blue-600 to-cyan-600 w-full">
+      <div className=" px-4 py-2 flex items-center justify-between">
         <div className=" flex justify-between items-center">
           <div className="flex items-center gap-2">
-          <BsBarChartLine className="text-white" />
-          <h1 className="text-white font-bold text-base">Meta Analytics</h1>
+            <BsBarChartLine className="text-white" />
+            <h1 className="text-white font-bold text-base">OniMeta</h1>
           </div>
-          
-        </div>{" "}
-        <nav></nav>
+        </div>
+        <nav>
+          <ul className="flex gap-2">
+            <li>
+              <Link
+                className="text-white text-base font-sans font-semibold hover:decoration-white hover:underline decoration-1 transition-all  underline-offset-2"
+                href={""}
+              >
+                +milionária
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white text-base font-sans font-semibold hover:decoration-white hover:underline decoration-1 transition-all  underline-offset-2"
+                href={""}
+              >
+                mega-sena
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white text-base font-sans font-semibold hover:decoration-white decoration-1 hover:underline transition-all  underline-offset-2"
+                href={""}
+              >
+                lotofácil
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white text-base font-sans font-semibold hover:decoration-white hover:underline decoration-1 transition-all  underline-offset-2"
+                href={""}
+              >
+                quina
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white text-base font-sans font-semibold hover:decoration-white hover:underline decoration-1 transition-all  underline-offset-2"
+                href={""}
+              >
+                lotomania
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white text-base font-sans font-semibold hover:decoration-white hover:underline decoration-1 transition-all  underline-offset-2"
+                href={""}
+              >
+                timemania
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white text-base font-sans font-semibold hover:decoration-white hover:underline decoration-1 transition-all  underline-offset-2"
+                href={""}
+              >
+                dupla sena
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white text-base font-sans font-semibold hover:decoration-white hover:underline decoration-1 transition-all  underline-offset-2"
+                href={""}
+              >
+                federal
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white text-base font-sans font-semibold hover:decoration-white hover:underline decoration-1 transition-all  underline-offset-2"
+                href={""}
+              >
+                dia de sorte
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white text-base font-sans font-semibold hover:decoration-white hover:underline decoration-1 transition-all  underline-offset-2"
+                href={""}
+              >
+                super sete
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
